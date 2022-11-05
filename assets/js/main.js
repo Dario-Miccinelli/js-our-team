2f0430b977984688cadc564e4b96b46bd3d5efe2
Dario-Miccinelli <poduzzo@hotmail.com>
2022-11-05 18:27:08 +0100

    end;

---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+
+/*
+MILESTONE 1:
+stampare su console le informazioni di nome, ruolo e la stringa della foto
+MILESTONE 2:
+stampare le stesse informazioni su DOM sottoforma di stringhe
+BONUS 1:
+trasformare la stringa foto in una immagine effettiva
+BONUS 2:
+organizzare i singoli membri in card/schede
+*/
+ 
+
+const team = [
+    {
+      name: 'Wayne Barnett',
+      role: 'Founder & CEO',
+      image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+      name: 'Angela Caroll',
+      role: 'Chief Editor',
+      image: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+      name: 'Walter Gordon',
+      role: 'Office Manager',
+      image: 'walter-gordon-office-manager.jpg',
+    },
+    {
+      name: 'Angela Lopez',
+      role: 'Social Media Manager',
+      image: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+      name: 'Scott Estrada',
+      role: 'Developer',
+      image: 'scott-estrada-developer.jpg',
+    },
+    {
+      name: 'Barbara Ramos',
+      role: 'Graphic Designer',
+      image: 'barbara-ramos-graphic-designer.jpg',
+    },
+  ];
+
+  const imageSearch = './assets/img/'
+
+
+  for (let i in team) {
+    let user = team[i];
+    console.log(imageSearch + user.image);
+    document.getElementById("teamOutput").innerHTML += `
+    <div class="bg-white mt-5 ">
+     <div>
+      <img  src=${imageSearch + user.image} >
+     </div>
+     <div class="text-center fs-4">${user.name}</div>
+     <div class="text-center">${user.role}</div>
+    </div>
+    
+    `
+
+}
